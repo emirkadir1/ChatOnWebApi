f5f307dd5ae731058b6904cb1ac9b67a4b5d2d7b
Onur Kablan <onurkablan0@gmail.com>
2023-04-23 14:13:40 +0300

    Dosya düzenlemeleri
    onur kaldırıldı.

--- Hubs/FriendHub.cs
+++ Hubs/FriendHub.cs
@@ -50,7 +50,6 @@ namespace ChatOnWebApi.Hubs
         }
         public async Task RemoveRequest(string sender, string reciever)
         {
-            var b = "onur";
             await Clients.Caller.SendAsync("Add-Friend");
         }
     }
